5115a759e53582d604c94c1615ca242e7a1f6ed7
Luke Morton <lukemorton.dev@gmail.com>
2018-12-09 13:11:01 +0000

    Move Post styling into component

--- src/templates/Post/index.js
+++ src/templates/Post/index.js
@@ -1,32 +1,15 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../../components/Layout'
+import Post from '../../components/Post'
 
-export default function PostTemplate ({ data }) {
-  const post = data.wordpressPost
-  const categoryNames = post.categories.map(c => c.name).join(', ')
-
+export default function PostPageTemplate ({ data }) {
   return (
     <Layout>
       <div className='container'>
         <div className='row'>
           <div className='col-md-8 offset-md-2' style={{ fontSize: '1.3rem' }}>
-            <article className='post'>
-              <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-
-              <div className='post__meta'>
-                <span className='post__avatar'>
-                  <img src={post.author.avatar_urls.wordpress_96} />
-                </span>
-
-                <span className='post__meta_text'>
-                  By {post.author.name}<br />
-                  {post.date} – {categoryNames}
-                </span>
-              </div>
-
-              <div dangerouslySetInnerHTML={{ __html: post.content }} />
-            </article>
+            <Post post={data.wordpressPost} />
           </div>
         </div>
       </div>
