fd9a4a2906b080ff148c2a9322f9d4db1d5f4042
Luke Morton <lukemorton.dev@gmail.com>
2018-12-09 13:38:46 +0000

    Tidy up Meta component

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -1,33 +1,23 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 
-export default function Meta ({ children }) {
+export default function Meta () {
   return (
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
-          }
-        }
-      `}
-      render={data => (
-        <>
-          <Helmet
-            title={data.site.siteMetadata.title}
-            meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
-            ]}
-          >
-            <html lang="en" />
-          </Helmet>
-          {children}
-        </>
-      )}
-    />
+    <Helmet>
+      <html lang='en' />
+      <title>Made Tech Blog</title>
+      <meta name='description' content='Read about the latest trends and techniques being used to deliver modern web applications. Featuring articles from the team at Made Tech and guest posts by selected industry experts.' />
+      <meta name='keywords' content='Made, Blog, Software, Agile, Rails, MadeTech, Made Tech, Spree Commerce, Spree, DevOps, Software Engineering, Continuous Delivery, Ruby on Rails' />
+      <meta property='og:url' content='https://www.madetech.com/blog' />
+      <meta property='og:title' content='Made Tech Blog' />
+      <meta property='og:description' content='Read about the latest trends and techniques being used to deliver modern web applications. Featuring articles from the team at Made Tech and guest posts by selected industry experts.' />
+
+      <link
+        href='http://www.madetech.com/feed'
+        rel='alternate'
+        type='application/atom+xml'
+        title='Latest blog posts from Made Tech'
+        />
+    </Helmet>
   )
 }
