2a16d679a8adeda9ee12c419c9555923fe8320d9
Luke Morton <lukemorton.dev@gmail.com>
2018-12-15 12:45:44 +0000

    Update to latest @madetech/frontend

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,35 +8,30 @@ export default function Layout ({ data, children }) {
     <div>
       <Meta />
 
-      <TopBar
-        links={[
-          <a href='https://www.madetech.com'>Who are Made Tech?</a>,
-          <a href='https://blog.madetech.com'>Blog</a>,
-          <a href='https://learn.madetech.com'>Learn</a>,
-          <a href='https://www.madetech.com/careers'>Careers</a>
-        ]}
-        />
+      <TopBar>
+        <a href='https://www.madetech.com'>Who are Made Tech?</a>
+        <a href='https://blog.madetech.com'>Blog</a>
+        <a href='https://learn.madetech.com'>Learn</a>
+        <a href='https://www.madetech.com/careers'>Careers</a>
+      </TopBar>
 
-      <Header
-        logoHref='/'
-        navLinks={[
-          <a href='https://www.madetech.com/agile-transformation' className='nav-link'>
-            Agile Team Transformation
-          </a>,
+      <Header logoHref='/'>
+        <a href='https://www.madetech.com/agile-transformation' className='nav-link'>
+          Agile Team Transformation
+        </a>
 
-          <a href='https://www.madetech.com/software-development' className='nav-link'>
-            Software Development
-          </a>,
+        <a href='https://www.madetech.com/software-development' className='nav-link'>
+          Software Development
+        </a>
 
-          <a href='https://www.madetech.com/continuous-delivery' className='nav-link'>
-            Continuous Delivery
-          </a>,
+        <a href='https://www.madetech.com/continuous-delivery' className='nav-link'>
+          Continuous Delivery
+        </a>
 
-          <a href='https://www.madetech.com/devops' className='nav-link'>
-            DevOps &amp; <br className='d-none d-lg-block' />Live Services
-          </a>
-        ]}
-        />
+        <a href='https://www.madetech.com/devops' className='nav-link'>
+          DevOps &amp; <br className='d-none d-lg-block' />Live Services
+        </a>
+      </Header>
 
       {children}
 
