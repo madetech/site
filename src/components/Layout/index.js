ce4e07aa57c22e7ff790fc7927a72f70364ccc30
Luke Morton <lukemorton.dev@gmail.com>
2018-12-19 21:51:14 +0000

    Remove hosts from URLs

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,26 +10,26 @@ export default function Layout ({ data, children }) {
       <Meta />
 
       <TopBar>
-        <a href='https://www.madetech.com'>Who are Made Tech?</a>
+        <a href='/'>Who are Made Tech?</a>
         <a href={withPrefix('/')}>Blog</a>
         <a href='https://learn.madetech.com'>Learn</a>
-        <a href='https://www.madetech.com/careers'>Careers</a>
+        <a href='/careers'>Careers</a>
       </TopBar>
 
       <Header logoHref={withPrefix('/')}>
-        <a href='https://www.madetech.com/agile-transformation' className='nav-link'>
+        <a href='/agile-transformation' className='nav-link'>
           Agile Team Transformation
         </a>
 
-        <a href='https://www.madetech.com/software-development' className='nav-link'>
+        <a href='/software-development' className='nav-link'>
           Software Development
         </a>
 
-        <a href='https://www.madetech.com/continuous-delivery' className='nav-link'>
+        <a href='/continuous-delivery' className='nav-link'>
           Continuous Delivery
         </a>
 
-        <a href='https://www.madetech.com/devops' className='nav-link'>
+        <a href='/devops' className='nav-link'>
           DevOps &amp; <br className='d-none d-lg-block' />Live Services
         </a>
       </Header>
