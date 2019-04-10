1bfee2c8c6543b30404865f19402673580c85e1b
Tom Taylor <tommotaylor@gmail.com>
2019-04-10 09:56:08 +0100

    Remove unused component

--- src/components/Contentful/HubSpotArea.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ContentfulHubSpotArea({ id }) {
-  return (
-    <div className={'contentful-hubspot-area'}>
-      <div id={'contentful-hubspot-form'} />
-    </div>
-  )
-}
