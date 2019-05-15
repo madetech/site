9195b52071c5d8a526bef2918444614f8e1ad40f
Luke Morton <lukemorton.dev@gmail.com>
2019-05-15 08:44:03 +0100

    Ensure smokes wait for all requests

--- test/smoke/contentfulPages.test.js
+++ test/smoke/contentfulPages.test.js
@@ -3,16 +3,21 @@ import { getAllPageSlugs } from '../support/contentful'
 
 describe('Smoke test for Contentful pages', function() {
   it('All published pages render with 200', async function() {
+    jest.setTimeout(10000)
+    expect.hasAssertions()
+
     const slugs = await getAllPageSlugs()
 
-    slugs.forEach(async slug => {
-      const response = await request({
-        uri: `https://www.madetech.com/${slug}`,
-        resolveWithFullResponse: true,
-      })
+    return Promise.all(
+      slugs.map(async slug => {
+        const response = await request({
+          uri: `${process.env.TEST_URL}/${slug}`,
+          resolveWithFullResponse: true,
+        })
 
-      expect(response.statusCode).toEqual(200)
-      expect(response.body).not.toMatch('Render Content Error')
-    })
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).not.toMatch('Render Content Error')
+      })
+    )
   })
 })
