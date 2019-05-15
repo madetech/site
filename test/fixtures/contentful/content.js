a7d2ae4dad3c35ef19ae0ee9642643e8c704372b
Luke Morton <lukemorton.dev@gmail.com>
2019-05-15 14:14:15 +0100

    Add comprehensive contentful fixture for page content

---
+++ test/fixtures/contentful/content.js
@@ -0,0 +1,269 @@
+export default function() {
+  return [
+    {
+      __typename: 'ContentfulJumbotron',
+      name: 'Continuous Delivery > Header',
+      background: null,
+      body: {
+        json: {
+          data: {},
+          content: [
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value: 'Continuous Delivery',
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'heading-2',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value:
+                    'Moving your application from development to production Cloud infrastructure, through a series of automated steps in a Continuous Delivery pipeline allows you to deliver software with more speed and less risk. ',
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'paragraph',
+            },
+            {
+              data: {},
+              content: [
+                { data: {}, marks: [], value: '', nodeType: 'text' },
+                {
+                  data: {
+                    uri:
+                      'https://cta-redirect.hubspot.com/cta/redirect/554916/2f1fadf1-170c-40aa-9a58-e011a030623b',
+                  },
+                  content: [
+                    {
+                      data: {},
+                      marks: [],
+                      value: 'Find out how we can help you get started',
+                      nodeType: 'text',
+                    },
+                  ],
+                  nodeType: 'hyperlink',
+                },
+                { data: {}, marks: [], value: '', nodeType: 'text' },
+              ],
+              nodeType: 'paragraph',
+            },
+          ],
+          nodeType: 'document',
+        },
+      },
+      columnWidth: 10,
+      columnOffset: 1,
+      textAlign: 'center',
+    },
+    {
+      __typename: 'ContentfulProse',
+      name: 'Continuous Delivery Onboarding > Prose',
+      body: {
+        json: {
+          data: {},
+          content: [
+            {
+              data: {},
+              content: [{ data: {}, marks: [], value: '', nodeType: 'text' }],
+              nodeType: 'paragraph',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value: 'Continuous Delivery Onboarding',
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'heading-2',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value:
+                    'Many organisations are realising significant value in adopting a Continuous Delivery strategy. Moving your application from development to production Cloud infrastructure, through a series of automated steps in a Continuous Delivery pipeline allows you to deliver software with more speed and less risk. Our team have extensive experience in delivering applications using a Continuous Delivery approach, and in supporting organisations, both with the compliance and organisational changes, as well as the technical implementation of delivery pipeline software, required to adopt Continuous Delivery.',
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'paragraph',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value: 'Who is this product for?',
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'heading-3',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value:
+                    'This product is ideally suited if you’re facing one of the following challenges:',
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'paragraph',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  content: [
+                    {
+                      data: {},
+                      content: [
+                        {
+                          data: {},
+                          marks: [],
+                          value:
+                            'You find it takes too long to move changes from development through to the production environment.',
+                          nodeType: 'text',
+                        },
+                      ],
+                      nodeType: 'paragraph',
+                    },
+                  ],
+                  nodeType: 'list-item',
+                },
+                {
+                  data: {},
+                  content: [
+                    {
+                      data: {},
+                      content: [
+                        {
+                          data: {},
+                          marks: [],
+                          value:
+                            'You carry an unacceptable amount of deployment risk, and may be subsequently nervous about deploying changes to your running application.',
+                          nodeType: 'text',
+                        },
+                      ],
+                      nodeType: 'paragraph',
+                    },
+                  ],
+                  nodeType: 'list-item',
+                },
+                {
+                  data: {},
+                  content: [
+                    {
+                      data: {},
+                      content: [
+                        {
+                          data: {},
+                          marks: [],
+                          value:
+                            'Your organisation is demanding more agility and faster software delivery.',
+                          nodeType: 'text',
+                        },
+                      ],
+                      nodeType: 'paragraph',
+                    },
+                  ],
+                  nodeType: 'list-item',
+                },
+                {
+                  data: {},
+                  content: [
+                    {
+                      data: {},
+                      content: [
+                        {
+                          data: {},
+                          marks: [],
+                          value:
+                            'You’re overly reliant on expensive manual testing every time you need to make a change to your production application.',
+                          nodeType: 'text',
+                        },
+                      ],
+                      nodeType: 'paragraph',
+                    },
+                  ],
+                  nodeType: 'list-item',
+                },
+                {
+                  data: {},
+                  content: [
+                    {
+                      data: {},
+                      content: [
+                        {
+                          data: {},
+                          marks: [],
+                          value:
+                            'You’re suffering from software quality problems.',
+                          nodeType: 'text',
+                        },
+                      ],
+                      nodeType: 'paragraph',
+                    },
+                  ],
+                  nodeType: 'list-item',
+                },
+              ],
+              nodeType: 'unordered-list',
+            },
+            {
+              data: {},
+              content: [{ data: {}, marks: [], value: '', nodeType: 'text' }],
+              nodeType: 'paragraph',
+            },
+            {
+              data: {},
+              content: [
+                {
+                  data: {},
+                  marks: [],
+                  value:
+                    "If you're looking to make the shift to Continuous Delivery, we're here to answer any questions you may have. Click on the button below to book some time to speak with one of our experts.",
+                  nodeType: 'text',
+                },
+              ],
+              nodeType: 'paragraph',
+            },
+          ],
+          nodeType: 'document',
+        },
+      },
+      columnWidth: 10,
+      columnOffset: 1,
+      image: null,
+      imageStyle: null,
+      textAlign: 'left',
+    },
+    {
+      __typename: 'ContentfulProse',
+      name: 'Continuous Delivery > CTA',
+      body: null,
+      columnWidth: 10,
+      columnOffset: 1,
+      image: null,
+      imageStyle: null,
+      textAlign: 'center',
+    },
+  ]
+}
