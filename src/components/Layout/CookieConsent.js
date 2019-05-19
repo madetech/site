8a1b33b6c229c6d55711b0a1efd880f563097c64
Luke Morton <lukemorton.dev@gmail.com>
2019-05-19 18:38:30 +0100

    Add cookie consent

---
+++ src/components/Layout/CookieConsent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Cookies from 'universal-cookie'
+
+export default class ContentfulHubSpotForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.cookies = new Cookies()
+    this.state = {
+      hideCookieConsent: true
+    }
+  }
+
+  componentDidMount() {
+    if (!this.cookies.get('hideCookieConsent')) {
+      this.setState({ hideCookieConsent: false })
+    }
+  }
+
+  handleClick() {
+    this.cookies.set('hideCookieConsent', true)
+    this.setState({ hideCookieConsent: true })
+  }
+
+  render() {
+    if (this.state.hideCookieConsent) return null
+
+    return (
+      <div className="fixed-bottom p-3" style={{ background: '#252525', color: '#ddd'}} >
+        <div className='container d-md-flex justify-content-between align-items-center'>
+          <div className='my-2'>We use cookies on this website to ensure you get the best experience.</div>
+
+          <button
+            className='btn btn-outline-light'
+            onClick={this.handleClick.bind(this)}
+          >
+            Okay, got it!
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
