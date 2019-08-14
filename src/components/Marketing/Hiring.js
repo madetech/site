2785ec79c8173fefb5761f5ad2f7e35963c84a8a
Luke Morton <lukemorton.dev@gmail.com>
2019-08-14 17:20:52 +0100

    Update text in job banner to not include specific roles

--- src/components/Marketing/Hiring.js
+++ src/components/Marketing/Hiring.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Hiring() {
   return (
     <p className="hiring">
-      We are hiring Software Engineers, Delivery Managers and Technical
-      Principals. Find out more about a{' '}
+      We are hiring! Find out more about a{' '}
       <a href="https://www.madetech.com/careers">career at Made Tech</a>.
     </p>
   )
