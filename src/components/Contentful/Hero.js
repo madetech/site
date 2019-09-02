1236182fbcaf5f03bdf18c4e703bc1a0a521fa9f
George Schena <george@madetech.com>
2019-09-02 10:05:10 +0100

    Add grid system for container

--- src/components/Contentful/Hero.js
+++ src/components/Contentful/Hero.js
@@ -7,18 +7,21 @@ export default function ContentfulHero({ pageTitle, sectionName, id, image }) {
   if (sectionName)
     sectionNameComponent = <p className="section-name">{sectionName}</p>
 
-  if (image) imageComponent = <img alt={image.title} src={image.fixed.src} />
+  if (image)
+    imageComponent = (
+      <img className="servies-image" alt={image.title} src={image.fixed.src} />
+    )
 
   return (
     <div className="contentful-hero" id={id}>
       <div className="container">
         <div className="row">
-          <p className="breadcrumb">Home/our services/blah blah</p>
-        </div>
-        <div className="row">
-          {sectionNameComponent}
-          <h1>{pageTitle}</h1>
-          {imageComponent}
+          <div className="col-5">
+            <p className="breadcrumbs">Home/our services/blah blah</p>
+            <div> {sectionNameComponent}</div>
+            <h1 className="page-title">{pageTitle}</h1>
+          </div>
+          <div className="col-7">{imageComponent}</div>
         </div>
       </div>
     </div>
