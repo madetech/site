2a8c730ac3ec6c963882f060ed732308d462d434
Matt Millar <matthew.millar@madetech.com>
2019-09-17 15:48:47 +0100

    Updated fonts, padding, spacing

--- src/helpers/threeSpaceToLineBreak.js
+++ src/helpers/threeSpaceToLineBreak.js
@@ -3,9 +3,13 @@ const reactStringReplace = require('react-string-replace')
 
 export default function threeSpaceToLineBreak(str, returnAsString = false) {
   if (!returnAsString) {
-    return reactStringReplace(str, '   ', (match, i) => <br key={i} />)
+    return reactStringReplace(str, '   ', (match, i) => (
+      <span key={i}>
+        <br key={i} />{' '}
+      </span>
+    ))
   }
 
   const regEx = new RegExp('   ', 'g')
-  return str.replace(regEx, '<br/>')
+  return str.replace(regEx, '<br/> ')
 }
