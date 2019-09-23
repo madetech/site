77db38721af162944a59b7798ceebd929b41db74
Matt Millar <matthew.millar@madetech.com>
2019-09-23 15:06:13 +0100

    Add ability to read images from rich content editor

--- src/helpers/documentToHtmlString.js
+++ src/helpers/documentToHtmlString.js
@@ -1,11 +1,30 @@
 import { BLOCKS } from '@contentful/rich-text-types'
 import { documentToHtmlString as _documentToHtmlString } from '@contentful/rich-text-html-renderer'
 
+const LOCALE = 'en-US' // contentful default
+
 export default function documentToHtmlString(doc) {
   return _documentToHtmlString(doc, {
     renderNode: {
-      [BLOCKS.PARAGRAPH]: (node, next) =>
-        `<p>${next(node.content).replace(/\n/g, '<br>')}</p>`,
+      [BLOCKS.PARAGRAPH]: (node, next) => {
+        return `<p>${next(node.content).replace(/\n/g, '<br>')}</p>`
+      },
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        if (node.data.target.fields) {
+          const { title, description, file } = node.data.target.fields
+          const mimeType = file[LOCALE].contentType
+          const mimeGroup = mimeType.split('/')[0]
+
+          switch (mimeGroup) {
+            case 'image':
+              return `<img title="${title ? title[LOCALE] : null}" alt="${
+                description ? description[LOCALE] : null
+              }" src="${file[LOCALE].url}" />`
+            default:
+              return `<span style={{ backgroundColor: 'red', color: 'white' }}>${mimeType} embedded asset</span>`
+          }
+        }
+      },
     },
   })
 }
