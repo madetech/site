9a9c5f698d07a7bb711925a797cafcd578ff9607
Matt Millar <matthew.millar@madetech.com>
2019-10-04 15:34:50 +0100

    Update the page-data version number in the webpack loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -192,24 +192,31 @@ exports.createPages = ({ graphql, actions }) => {
   ])
 }
 
-exports.onPostBuild = async () => {
-  const publicPath = path.join(__dirname, 'public')
-  const hash = md5(`${new Date().getTime()}`)
+const hash = md5(`${new Date().getTime()}`)
 
-  const htmlAndJSFiles = glob.sync(`${publicPath}/**/*.{html,js}`)
-  for (let file of htmlAndJSFiles) {
-    const stats = await util.promisify(fs.stat)(file)
-    if (!stats.isFile()) {
-      continue
-    }
-
+const addPageDataVersion = async file => {
+  const stats = await util.promisify(fs.stat)(file)
+  if (stats.isFile()) {
     console.log(`Adding version to page-data.json in ${file}..`)
     let content = await util.promisify(fs.readFile)(file, 'utf8')
     const result = content.replace(
-      /page-data.json/g,
+      /page-data.json(\?v=[a-f0-9]{32})?/g,
       `page-data.json?v=${hash}`
     )
     await util.promisify(fs.writeFile)(file, result, 'utf8')
+  }
+}
+
+exports.onPostBootstrap = async () => {
+  const loader = path.join(__dirname, 'node_modules/gatsby/cache-dir/loader.js')
+  await addPageDataVersion(loader)
+}
+
+exports.onPostBuild = async () => {
+  const publicPath = path.join(__dirname, 'public')
+  const htmlAndJSFiles = glob.sync(`${publicPath}/**/*.{html,js}`)
+  for (let file of htmlAndJSFiles) {
+    await addPageDataVersion(file)
   }
 }
 
@@ -217,7 +224,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
   actions.setWebpackConfig({
     devtool: 'eval-source-map',
     resolve: {
-      alias: { 'react-dom': '@hot-loader/react-dom'  }
-    }
+      alias: { 'react-dom': '@hot-loader/react-dom' },
+    },
   })
 }
