8ec2f79a1572efccaecf7f7baac7c837d26ffe07
Matt Millar <matthew.millar@madetech.com>
2019-10-14 16:01:27 +0100

    Fixed the soft-hyphen insertion function

--- src/helpers/threeHyphenToSoftHyphen.js
+++ src/helpers/threeHyphenToSoftHyphen.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react'
 const reactStringReplace = require('react-string-replace')
 
 export default function threeHyphenToSoftHyphen(str, returnAsString = false) {
   if (!returnAsString) {
-    let replacements = 0
-    return reactStringReplace(str, '---', match => (
-      <Fragment key={`${match} ${replacements++}`}>&shy;</Fragment>
-    ))
+    return reactStringReplace(str, /(.*---.*)/g, m => {
+      const regEx = new RegExp(/-{3}/, 'g')
+      return m.replace(regEx, '\u00AD')
+    })
   }
 
   const regEx = new RegExp(/-{3}/, 'g')
