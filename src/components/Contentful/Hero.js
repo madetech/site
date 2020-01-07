ea14d224e85348f54f62372b01c2d72e90304ae0
Kat <katrina@madetech.com>
2020-01-07 12:32:39 +0000

    Adding Hero component from the frontend

--- src/components/Contentful/Hero.js
+++ src/components/Contentful/Hero.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import threeSpaceToLineBreak from '../../helpers/threeSpaceToLineBreak'
 import threeHyphenToSoftHyphen from '../../helpers/threeHyphenToSoftHyphen'
+import { Hero } from '@madetech/frontend'
 
 export default function ContentfulHero({
   id,
@@ -67,37 +68,39 @@ export default function ContentfulHero({
   parsedTitle = threeHyphenToSoftHyphen(parsedTitle)
 
   return (
-    <div className={`contentful-hero ${backgroundColourStyle}`} id={id}>
-      <div className="container">
-        <div className="contentful-hero__row">
-          <div className="row">
-            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
-              <div className="contentful-hero__text-box">
-                {pageBreadcrumbComponent}
-                <h1
-                  className={`contentful-hero__page-title ${textSizeStyle} ${textColourStyle}`}
-                >
-                  {parsedTitle}
-                </h1>
-                {headerTextComponent}
+    <div className={`${backgroundColourStyle}`} id={id}>
+      <Hero>
+        <div className="container">
+          <div className="contentful-hero__row">
+            <div className="row">
+              <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
+                <div className="contentful-hero__text-box">
+                  {pageBreadcrumbComponent}
+                  <h1
+                    className={`contentful-hero__page-title ${textSizeStyle} ${textColourStyle}`}
+                  >
+                    {parsedTitle}
+                  </h1>
+                  {headerTextComponent}
+                </div>
               </div>
-            </div>
-            <div
-              className={`col-xl-6 col-lg-6 col-md-6 d-none d-md-block contentful-hero__image hero_${textColourStyle}`}
-              style={{
-                backgroundImage:
-                  'url(' +
-                  headerImage.fixed.src +
-                  '), url(' +
-                  headerImage.resize.src +
-                  ')',
-              }}
-            >
-              {links}
+              <div
+                className={`col-xl-6 col-lg-6 col-md-6 d-none d-md-block contentful-hero__image hero_${textColourStyle}`}
+                style={{
+                  backgroundImage:
+                    'url(' +
+                    headerImage.fixed.src +
+                    '), url(' +
+                    headerImage.resize.src +
+                    ')',
+                }}
+              >
+                {links}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Hero>
     </div>
   )
 }
