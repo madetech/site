449769f77c31a1ae5237370ce377ad5a37d1abbb
Ninamma <Ninamma@madetech.com>
2020-03-02 15:49:56 +0000

    Trying to get react-burger-menu to work

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,6 +3,7 @@ import '../../all.scss'
 import Meta from './Meta'
 import Scripts from './Scripts'
 import { Footer, Header, SiteMap, TopBar } from '@madetech/frontend'
+import Example from '../Contentful/Menu'
 
 export default function Layout({
   children,
@@ -36,6 +37,8 @@ export default function Layout({
         <a href="https://learn.madetech.com">Learn</a>
         <a href="/resources/books">Books</a>
       </TopBar>
+
+      <Example classes={'contentful-mobile-menu'}></Example>
 
       <Header constrainLinkWidth logoHref="/" scrollable>
         <a href="/our-services">Our Services</a>
