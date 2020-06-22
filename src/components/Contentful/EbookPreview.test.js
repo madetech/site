624099ed129f8e9d4ad65f4c9c431a900e20d263
Tony Griffin <tony@madetech.com>
2020-06-22 17:00:37 +0100

    Add working tests for EbookPreview

--- src/components/Contentful/EbookPreview.test.js
+++ src/components/Contentful/EbookPreview.test.js
@@ -3,18 +3,33 @@ import { shallow } from 'enzyme'
 import EbookPreview from './EbookPreview'
 
 describe('EbookPreview', () => {
-  const component = shallow(
-    <EbookPreview
-      alignItems="Expect start"
-      content={[{ name: 'Expect download' }, { name: 'Expect showcase' }]}
-      customClasses={['book__showcase', 'book']}
-      id="24e7a389-e2f0-51d7-8519-4ce5a594fcb7"
-      layout="Expect normal"
-      style="Expect red"
-    />
-  )
-
   it('Renders the component with information provided by props', () => {
-    expect(component.find({ 'data-test': 'ebook' }).length.toBe(1))
+    const component = shallow(
+      <EbookPreview
+        alignItems="Expect start"
+        content={{
+          content: [{ name: 'Expect download' }, { name: 'Expect showcase' }],
+        }}
+        customClasses={['book__showcase', 'book']}
+        id="24e7a389-e2f0-51d7-8519-4ce5a594fcb7"
+        layout="Expect normal"
+        style="Expect red"
+      />
+    )
+    expect(component.find({ 'data-test': 'ebook' }).length).toBe(1)
+  })
+
+  it('Returns null when props is null', () => {
+    const component = shallow(
+      <EbookPreview
+        alignItems="Expect start"
+        content={null}
+        customClasses={['book__showcase', 'book']}
+        id="24e7a389-e2f0-51d7-8519-4ce5a594fcb7"
+        layout="Expect normal"
+        style="Expect red"
+      />
+    )
+    expect(component.find({ 'data-test': 'ebook' }).length).toBe(0)
   })
 })
