01fe60bd43779ddcd0a33890a6be237f5c6ca752
Archita Nayak <archita.nayak@bacancytechnology.com>
2020-12-17 13:46:01 +0530

    state replaced with city

--- src/components/JobsBoard/index.js
+++ src/components/JobsBoard/index.js
@@ -7,7 +7,7 @@ export default function JobBoard() {
     <Helmet>
       <script src='https://www.workable.com/assets/embed.js' type='text/javascript' onLoad={`
           whr(document).ready(function(){
-            whr_embed(457904, {detail: 'titles', base: 'jobs', zoom: 'state', grouping: 'departments'})
+            whr_embed(457904, {detail: 'titles', base: 'jobs', zoom: 'city', grouping: 'departments'})
           })
         `}></script>
     </Helmet>
